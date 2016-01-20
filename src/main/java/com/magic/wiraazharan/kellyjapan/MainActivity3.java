f31f4e34f9227e2037cc85af267062c8feffaff2
wiraazharan <wiraazharan@gmail.com>
2016-01-20 14:28:34 +0800

    Insert two button to MainActivity3

--- src/main/java/com/magic/wiraazharan/kellyjapan/MainActivity3.java
+++ src/main/java/com/magic/wiraazharan/kellyjapan/MainActivity3.java
@@ -7,41 +7,73 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 import java.util.Timer;
 
-public class MainActivity3 extends Activity {
-
-    Button buttonyes;
-    Button buttonno;
-
+public class MainActivity3 extends Activity implements View.OnClickListener{
+    
+    Button buttonYes;
+    Button buttonNo;
+    
     MyCountDownTimer timer;
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
-
+        
         timer = new MyCountDownTimer(4000,1000);
-        //timer.start();
+        
+        //initialization of button
+        buttonYes = (Button)findViewById(R.id.one);
+        buttonNo = (Button)findViewById(R.id.two);
+        
+        //implement onClickListener
+        buttonYes.setOnClickListener(this);
+        buttonNo.setOnClickListener(this);
+        
+        timer.start();
     }
-
-
+    
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.one:
+                Intent myIntent3 = new Intent(MainActivity3.this,MainActivity.class);
+                startActivity(myIntent3);
+                finish();
+                break;
+                
+            case R.id.two:
+                Intent myIntent4 = new Intent(MainActivity3.this,MainActivity.class);
+                startActivity(myIntent4);
+                finish();
+                break;
+                
+        }
+    }
+    
+    
     private class MyCountDownTimer extends CountDownTimer{
-
+        
         public MyCountDownTimer(long millisinfuture , long countDownInterval){
             super(millisinfuture,countDownInterval);
         }
-
+        
         @Override
         public void onTick(long l) {
-
+            
         }
-
+        
         @Override
         public void onFinish() {
             Intent myIntent3 = new Intent(MainActivity3.this,MainActivity.class);
+            startActivity(myIntent3);
         }
     }
+    
+    
 }
