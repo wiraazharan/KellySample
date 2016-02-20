aa34163b24bf8941c3b4c50d8107b09e2d0cc31a
wiraazharan <wiraazharan@gmail.com>
2016-02-20 19:59:20 +0800

    Error get fixed : assume the error due to garbage collector. 	- Create new class to hold the variable 	- Instantiate the class that holds the variable 	- Use two method to insert the data and get the data from the variable inside the newly created class 	- After retieve the class, pass the variable to local variable called "k" 	- Toast the k value everytime the loopimageArray() being executed

---
+++ src/main/java/com/magic/wiraazharan/kellyjapan/Data.java
@@ -0,0 +1,18 @@
+package com.magic.wiraazharan.kellyjapan;
+
+/**
+ * Created by wiraazharan on 20/02/16.
+ */
+public class Data {
+   public  int k = 0;
+
+   public void insert(){
+       k = k+1;
+   }
+
+   public int getdata(){
+       return k;
+   }
+
+
+}
--- src/main/java/com/magic/wiraazharan/kellyjapan/MainActivity2.java
+++ src/main/java/com/magic/wiraazharan/kellyjapan/MainActivity2.java
@@ -21,251 +21,6 @@ import android.widget.Toast;
 public class MainActivity2 extends Activity implements View.OnClickListener {
 
 
-//    ImageView imageView1;
-//
-//    int[] question;
-//
-//    int[] answer;
-//
-//    int i;
-//
-//    int[] imageArray;
-//
-//    Button buttonYes;
-//
-//    Button buttonNo;
-//
-//    MyCountDownTimer timer;
-//
-//    @Override
-//
-//    protected void onCreate(Bundle savedInstanceState) {
-//
-//        super.onCreate(savedInstanceState);
-//
-//        setContentView(R.layout.activity_main2);
-//
-//        //Initialization of button
-//
-//        buttonYes = (Button)findViewById(R.id.buttonYes);
-//
-//        buttonYes.setVisibility(View.INVISIBLE);
-//
-//        buttonNo = (Button)findViewById(R.id.buttonNo);
-//
-//        buttonNo.setVisibility(View.INVISIBLE);
-//
-//        //Implement onClickListener
-//
-//        buttonYes.setOnClickListener(this);
-//
-//        buttonNo.setOnClickListener(this);
-//
-////Question array
-//
-//        int[] question = {R.drawable.image1, R.drawable.image1,R.drawable.image1,R.drawable.image1,
-//                R.drawable.image1, R.drawable.image1,R.drawable.image1,R.drawable.image1,
-//                R.drawable.image1, R.drawable.image1};
-//
-//
-////Answer array
-//        int[] answer = {R.drawable.greycross, R.drawable.greycross,R.drawable.greycross,R.drawable.greycross,
-//                R.drawable.greycross, R.drawable.greycross,R.drawable.greycross,R.drawable.greycross,
-//                R.drawable.greycross, R.drawable.greycross};
-//
-//
-////ImageView array
-//
-//        int[] imageArray = {R.drawable.image1, R.drawable.image2,question[i],R.drawable.image1,
-//                answer[i]};
-//
-//        //Loop for imageArray
-//
-//        for ( i = 0;  i<imageArray.length; i++){
-//
-//            imageView1.setImageResource(imageArray[0]);
-//
-//            for (int j=0; j<question.length; j++){
-//
-//                for (int k=0; k<answer.length; k++){
-//
-////PreProcess (img1, img2, ques, img1, ans)
-//
-//                    PreProcess(15000, 15500, 17000, 24000, 26000);
-//
-//
-//
-////TimeToShowButton
-//
-//                    TimeToShowButton(29000);
-//
-//
-//
-////Time for one loop to end before continuing another loop
-//
-//                    timer = new MyCountDownTimer(34000, 1000);
-//
-//                    timer.start();
-//
-//                }
-//
-//            }
-//
-//        }
-//
-//    }
-//
-////Timer for PreProcess
-//
-//    public void PreProcess(int timer1, int timer2,  int timer3, int timer4, int timer5) {
-//
-//
-//
-//        RunTimer(timer1, 1);
-//
-//        RunTimer(timer2, 2);
-//
-//        RunTimer(timer3, 2);
-//
-//        RunTimer(timer4, 3);
-//
-//        RunTimer(timer5, 4);
-//
-//    }
-//
-//
-//
-////Timer to show button
-//
-//    private void TimeToShowButton (int mytimer1){
-//
-//
-//
-//        new CountDownTimer(mytimer1, 1000) {
-//
-//
-//
-//            public void onTick(long millisUntilFinished) {
-//
-//
-//
-//            }
-//
-//
-//
-//            public void onFinish() {
-//
-//                buttonYes.setVisibility(View.VISIBLE);
-//
-//                buttonNo.setVisibility(View.VISIBLE);
-//
-////Button visible at 29000ms
-//
-//            }
-//
-//        }.start();
-//
-//    }
-//
-//    //Timer to display multiple images with different time interval
-//
-//    private void RunTimer(int myTimer, final int position){
-//
-//
-//
-//        new CountDownTimer(myTimer, 1000) {
-//
-//
-//
-//            public void onTick(long millisUntilFinished) {
-//
-//            }
-//
-//
-//
-//            public void onFinish() {
-//
-//                imageView1.setImageResource(imageArray[position]);
-//
-//                imageView1.setVisibility(View.VISIBLE);
-//
-//            }
-//
-//        }.start();
-//
-//    }
-//
-//
-//
-////Implement onClick on button
-//
-////Prompt to button click, loop continue
-//
-//    public void onClick(View view) {
-//
-//        switch (view.getId())
-//
-//        {
-//
-//            case R.id.buttonYes:
-//
-//                //CONTINUE LOOP
-//
-//                break;
-//
-//
-//            case R.id.buttonNo:
-//
-//                //CONTINUE LOOP
-//
-//                break;
-//
-//        }
-//
-//    }
-//
-//
-//
-//    //If there is no button click within 2.0s, loop continue
-//
-//    private class MyCountDownTimer extends CountDownTimer{
-//
-//
-//
-//        public MyCountDownTimer(long millisinfuture , long countDownInterval){
-//
-//            super(millisinfuture,countDownInterval);
-//
-//        }
-//
-//
-//
-//        @Override
-//
-//        public void onTick(long l) {
-//
-//
-//
-//        }
-//
-//
-//
-//        @Override
-//
-//        public void onFinish() {
-//
-//            //CONTINUE LOOP
-//
-//        }
-//
-//    }
-//
-//
-//
-//
-//
-//}
-
     ImageView imageView1;
 
     int[] question;
@@ -274,10 +29,12 @@ public class MainActivity2 extends Activity implements View.OnClickListener {
 
     int[] imageArray;
 
-    int k = 0;
+    public int k = 0;
 
     Button buttonYes;
     Button buttonNo;
+
+    Data mydata = new Data();
 
     MyCountDownTimer timer;
 
@@ -322,11 +79,13 @@ public class MainActivity2 extends Activity implements View.OnClickListener {
         answer[6] = R.drawable.image4;
         answer[7] = R.drawable.image4;
         answer[8] = R.drawable.image4;
-        answer[9] = R.drawable.image4;
+        answer[9] = R.drawable.image1;
 
 
 
         imageView1 = (ImageView) findViewById(R.id.imageView1);
+
+
 
         loopimageArray();
 
@@ -334,7 +93,11 @@ public class MainActivity2 extends Activity implements View.OnClickListener {
 //        }
 //    }
 
+
+
     public void loopimageArray() {
+
+        k = mydata.getdata();
 
         //ImageView array
         imageArray = new int[5];
@@ -345,8 +108,8 @@ public class MainActivity2 extends Activity implements View.OnClickListener {
         imageArray[4] = answer[k];
 
 
+        imageView1.setImageResource(imageArray[0]);
 
-            imageView1.setImageResource(imageArray[0]);
 
 
 //PreProcess (img1, img2, ques, img1, ans)
@@ -356,8 +119,11 @@ public class MainActivity2 extends Activity implements View.OnClickListener {
             TimeToShowButton(29000);
 
 
+
             timer = new MyCountDownTimer(34000, 1000);
             timer.start();
+
+
         //}
     }
 
@@ -416,7 +182,8 @@ public class MainActivity2 extends Activity implements View.OnClickListener {
                     startActivity(i);
                 }else
                 {
-                    k++;
+                    mydata.insert();
+                    imageArray = null;
                     loopimageArray();
                     buttonYes.setVisibility(View.INVISIBLE);
                     buttonNo.setVisibility(View.INVISIBLE);
@@ -431,7 +198,8 @@ public class MainActivity2 extends Activity implements View.OnClickListener {
                     startActivity(i);
                 }else
                 {
-                    k++;
+                    mydata.insert();
+                    imageArray = null;
                     loopimageArray();
                     buttonYes.setVisibility(View.INVISIBLE);
                     buttonNo.setVisibility(View.INVISIBLE);
@@ -462,7 +230,10 @@ public class MainActivity2 extends Activity implements View.OnClickListener {
                 startActivity(i);
             }else
             {
-                k++;
+                Toast.makeText(getApplicationContext(),k+" ",Toast.LENGTH_SHORT).show();
+                //k++;
+                mydata.insert();
+                imageArray = null;
                 loopimageArray();
                 buttonYes.setVisibility(View.INVISIBLE);
                 buttonNo.setVisibility(View.INVISIBLE);
